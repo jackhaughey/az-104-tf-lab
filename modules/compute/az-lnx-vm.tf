8c453af031ee2b825a2c67d3347871e8a6c14590
Jack Haughey <jack.haughey@gmail.com>
2025-10-14 14:40:18 +0100

    changes to vm entry

--- modules/compute/az-lnx-vm.tf
+++ modules/compute/az-lnx-vm.tf
@@ -1,25 +1,8 @@
-resource "azurerm_resource_group" "ubuntu1-rg" {
-  name     = "ubuntu-vm-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_network_interface" "test-ubu-1-ni" {
-  name                = "test-ubu-1-ni"
-  location            = azurerm_resource_group.ubuntu1-rg.location
-  resource_group_name = azurerm_resource_group.ubuntu1-rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.vm-subnet-1.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "test-ubu-1-vm" {
   name                = "test-ubu-1-vm"
   resource_group_name = azurerm_resource_group.ubuntu1-rg.name
   location            = azurerm_resource_group.ubuntu1-rg.location
-  size                = "Standard_F2"
+  size                = "Standard_B2pts_v2"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.test-ubu-1-ni.id,
